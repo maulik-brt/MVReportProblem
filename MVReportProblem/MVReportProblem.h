4485f55eb7acb2737c73977933cd9b5f05c037d8
Maulik Vekariya <maulik.vekariya@bluereeftech.com>
2020-09-17 14:58:44 +0530

    Added license file

---
+++ MVReportProblem/MVReportProblem.h
@@ -0,0 +1,19 @@
+//
+//  MVReportProblem.h
+//  MVReportProblem
+//
+//  Created by Maulik Vekariya on 17/09/20.
+//  Copyright © 2020 BlueWhale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MVReportProblem.
+FOUNDATION_EXPORT double MVReportProblemVersionNumber;
+
+//! Project version string for MVReportProblem.
+FOUNDATION_EXPORT const unsigned char MVReportProblemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVReportProblem/PublicHeader.h>
+
+
